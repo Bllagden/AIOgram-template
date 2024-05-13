8acd7c5323574ba890aacab7cf70d59437ba863e
Bllagden <bllagden99@gmail.com>
2024-05-13 17:43:32 +0400

    Add keyboard_remove to message_answer

--- src/app/adapters/telegram_bot/routers/echo.py
+++ src/app/adapters/telegram_bot/routers/echo.py
@@ -1,7 +1,7 @@
 from datetime import UTC, datetime
 
 from aiogram import F, Router, html
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardRemove
 
 router = Router()
 
@@ -13,4 +13,7 @@ async def echo_with_time_handler(message: Message) -> None:
         f"Команда не распознана\nСоздано по UTC в {time_now}",  # noqa: RUF001
     )
     print("echo")  # noqa: T201
-    await message.answer(f"{message.text}\n\n{added_text}")
+    await message.answer(
+        text=f"{message.text}\n\n{added_text}",
+        reply_markup=ReplyKeyboardRemove(),
+    )
